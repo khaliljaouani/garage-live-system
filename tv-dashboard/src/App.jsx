7701aebaf50ceaba86eb93d0d42e65c7b6dad61b
khalil <khalil.jaouani29@gmail.com>
2026-04-30 14:11:54 +0200

    fix: frontend3

--- tv-dashboard/src/App.jsx
+++ tv-dashboard/src/App.jsx
@@ -75,7 +75,7 @@ export default function App() {
           <p style={{ gridColumn: "span 2", textAlign: "center", color: "#94a3b8" }}>Aucun véhicule...</p>
         )}
 
-        {cars.map((car) => (
+        {[...cars].reverse().map((car) => (
           <div key={car._id} style={{
             position: "relative",
             borderRadius: "10px",
